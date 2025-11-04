b70473d57b4aacce69cdebf1dfa0addacf2effa1
Durvesh Kumar <durvesh88999@gmail.com>
2025-11-04 17:34:17 +0530

    Update router

--- apps/auth-service/src/routers/user/index.ts
+++ apps/auth-service/src/routers/user/index.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express';
-import { sendOTP, userLoginOTP } from '../../controllers/user';
+import { logOut, sendOTP, userLoginOTP } from '../../controllers/user';
 
 const userRouter:Router = express.Router();
 
 userRouter.post('/login', userLoginOTP)
 userRouter.post('/send-otp', sendOTP)
+userRouter.get('/logout', logOut)
 
 export default userRouter;
