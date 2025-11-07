99b2b2ec58c9b70907d241398a844e5a6dbbc15d
Durvesh Kumar <durvesh88999@gmail.com>
2025-11-08 02:10:15 +0530

    update domane name

---
+++ help.sh
@@ -0,0 +1,15 @@
+sudo certbot certonly --standalone \
+  -d redbasketapp.hopto.org \
+  -d https://redbasketapp.hopto.org \
+  --email redbasketapp@gmail.com \
+  --agree-tos \
+  --non-interactive
+
+
+sudo certbot certonly --standalone --dry-run \
+  -d redbasketapp.com \
+  -d www.redbasketapp.com \
+  -d admin.redbasketapp.com \
+  --email redbasketapp.com \
+  --agree-tos \
+  --non-interactive
